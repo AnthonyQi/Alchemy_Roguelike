0ae67604730fd9848c8216aa68d5845329b01b14
AnthonyQi <aq.951841@gmail.com>
2025-10-13 14:15:32 -0400

    yay

--- Assets/Minigames/IngredientInferno_6ix7even/Scripts/ProjController_6ix7even.cs
+++ Assets/Minigames/IngredientInferno_6ix7even/Scripts/ProjController_6ix7even.cs
@@ -1,33 +1,35 @@
-
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-/*
-    This is an example script part of the debug minigame
-
-    The purpose of it is to show you how to properly deal with input
-    and use the provided MinigameManager.cs class
-*/
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class ProjController_6ix7even : MonoBehaviour
 {
- private Rigidbody2D rb;
-   public GameObject projectile;
-   public float atkSpeed = 0.5f;
-   public float ballSpeed = 700f;
+    public GameObject projectile;
+    public float atkSpeed = 0.5f;
+    public float ballSpeed = 20f;
 
-    void Update() {
-        if (Input.GetButtonDown("Fire1")) {
+    void Update() 
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
             Fire();
         }
     }
-    void Fire() {
-            Vector3 mousePos = Input.mousePosition;
-            Rigidbody2D fireball = Instantiate(rb, mousePos);
-            fireball.GetComponent<Rigidbody2D>().AddForce(mousePos.x*ballSpeed);
-            coolDown = Time.time + attackSpeed;
-            return;
-    }
 
+    void Fire() 
+    {
+        // Get mouse position in world space
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f; // ensure z=0 for 2D
+
+        // Instantiate the projectile at the player's position
+        GameObject fireball = Instantiate(projectile, transform.position, Quaternion.identity);
+
+        // Calculate direction
+        Vector2 direction = (mousePos - transform.position).normalized;
+
+        // Add force
+        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * ballSpeed, ForceMode2D.Impulse);
+    }
 }
